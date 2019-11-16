b0f72035a61f7afd1cd3f77ced3ecf58c9af7d4e
natuong2017 <natuong2017@gmail.com>
2019-11-17 01:42:48 +0700

    Create layout for dicom viewer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DicomViewerComponent } from './views/dicom-viewer/dicom-viewer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DicomViewerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/views/dicom-viewer/dicom-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dicom-viewer',
+  templateUrl: './dicom-viewer.component.html',
+  styleUrls: ['./dicom-viewer.component.scss']
+})
+export class DicomViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
